a9bd843d7627494db210313e4ccbf776c22c25c3
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-6.local>
2019-11-29 16:55:06 -0500

    complete

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -1,7 +1,46 @@
 package com.zipcodewilmington.arrayutility;
 
+import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility <T>{
+
+    private T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToCheck) {
+        array = Stream.concat(Arrays.stream(array), Arrays.stream(arrayToMerge)).toArray(size -> Arrays.copyOf(array, size));
+        return getNumberOfOccurrences(valueToCheck);
+    }
+
+    public T getMostCommonFromMerge( T[] arrayToMerge) {
+
+        array = Stream.concat(Arrays.stream(array), Arrays.stream(arrayToMerge))
+                .toArray(size -> Arrays.copyOf(array, size));
+
+        return Arrays.stream(array)
+                .max((Comparator.comparingInt(this::getNumberOfOccurrences)))
+                .orElse(null);
+    }
+
+    public Integer getNumberOfOccurrences( T valueToCheck) {
+        return((int) Arrays.stream(array)
+                .filter(val -> val == valueToCheck)
+                .count());
+    }
+
+    public T[] removeValue(T valueToRemove) {
+        return Arrays.stream(array)
+                .filter(element -> !element.equals(valueToRemove))
+                .toArray(size -> Arrays.copyOf(array, size));
+    }
 }
